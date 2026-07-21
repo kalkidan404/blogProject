4fe15e171d33bafd9a6f5ec83503b1523643f0c0
kalkidan404 <kalrokal1621@gmail.com>
2026-07-21 08:05:44 +0300

    prisma and jwt connection done

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,37 +1,11 @@
-const express = require("express");
-
-const app = express();
-
-/*
-|--------------------------------------------------------------------------
-| Global Middleware
-|--------------------------------------------------------------------------
-*/
-
-// Parse incoming JSON requests
+const express= require("express");
+const cors= require("cors");
+const app=express();
+app.use(cors());
 app.use(express.json());
-
-// Parse HTML form data
-app.use(express.urlencoded({ extended: true }));
-
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-*/
-
-// Temporary route to verify the API is working
-app.get("/", (req, res) => {
+ app.get("/",(req,res)=>{
     res.json({
-        success: true,
-        message: "Blog API is running 🚀",
+        message:"Blog API is running",
     });
-});
-
-/*
-|--------------------------------------------------------------------------
-| Export App
-|--------------------------------------------------------------------------
-*/
-
-module.exports = app;
+ });
+ module.exports=app;
